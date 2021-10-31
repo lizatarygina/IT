b71f76a08dac1fc48d59151d6dabfd3f6e7f900a
Quartery <andreychik2017@yandex.ru>
2021-10-31 13:44:26 +0300

    31.10.21 Введение в ИТ

--- calculator/main.py
+++ calculator/main.py
@@ -2,207 +2,118 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton
 import sys
 
 calc = 0
-pr = 0
-a = ''
+pd = 0
+numb = ''
 
 
 def prov():
-    global calc, pr, a
-    if pr == 0:
-        calc += int(a)
-    elif pr == 1:
-        calc += int(a)
-    elif pr == 2:
-        calc -= int(a)
-    elif pr == 3:
-        calc *= int(a)
-    elif pr == 4:
-        calc //= int(a)
-    a = ''
+    global calc, pd, numb
+    if pd == 0:
+        calc += int(numb)
+    elif pd == 1:
+        calc += int(numb)
+    elif pd == 2:
+        calc -= int(numb)
+    elif pd == 3:
+        calc *= int(numb)
+    elif pd == 4:
+        calc //= int(numb)
+    numb = ''
 
 
 class Window(QWidget):
 
     def __init__(self):
         super(Window, self).__init__()
-        self.btnc = QPushButton(self)
-        self.btnr = QPushButton(self)
-        self.btns = QPushButton(self)
-        self.btny = QPushButton(self)
-        self.btnd = QPushButton(self)
-        self.btn0 = QPushButton(self)
-        self.btn9 = QPushButton(self)
-        self.btn8 = QPushButton(self)
-        self.btn7 = QPushButton(self)
-        self.btn6 = QPushButton(self)
-        self.btn5 = QPushButton(self)
-        self.btn4 = QPushButton(self)
-        self.btn3 = QPushButton(self)
-        self.btn2 = QPushButton(self)
-        self.btn1 = QPushButton(self)
-        self.btn = QPushButton(self)
         self.le = QLineEdit(self)
         self.set_ui()
 
     def set_ui(self):
-        self.setGeometry(200, 200, 210, 250)
+        self.setGeometry(200, 200, 160, 200)
         self.setWindowTitle('Калькулятор')
-        self.le.setGeometry(30, 70, 120, 30)
+        self.le.setGeometry(0, 0, 120, 40)
 
-        self.btn.setGeometry(150, 100, 30, 120)
-        self.btn.setText('=')
-        self.btn.clicked.connect(self.equal_click_event)
+        for i in range(10):
+            self.btn = QPushButton(str(i), self)
+            if i == 0:
+                self.btn.setGeometry(40, 160, 40, 40)
+            elif i <= 3:
+                self.btn.setGeometry(40 * (i - 1), 120, 40, 40)
+            elif i <= 6:
+                self.btn.setGeometry(40 * (i - 4), 80, 40, 40)
+            elif i <= 9:
+                self.btn.setGeometry(40 * (i - 7), 40, 40, 40)
+            self.btn.clicked.connect(lambda checked=None, j=i: self.click_event(j))
 
-        self.btn1.setGeometry(30, 100, 30, 30)
-        self.btn1.setText('1')
-        self.btn1.clicked.connect(self.click_event_1)
+        self.btne = QPushButton('=', self)
+        self.btne.setGeometry(120, 120, 40, 80)
+        self.btne.clicked.connect(self.click_event_e)
 
-        self.btn2.setGeometry(60, 100, 30, 30)
-        self.btn2.setText('2')
-        self.btn2.clicked.connect(self.click_event_2)
+        self.btnp = QPushButton('+', self)
+        self.btnp.setGeometry(120, 40, 40, 40)
+        self.btnp.clicked.connect(self.click_event_p)
 
-        self.btn3.setGeometry(90, 100, 30, 30)
-        self.btn3.setText('3')
-        self.btn3.clicked.connect(self.click_event_3)
+        self.btnm = QPushButton('-', self)
+        self.btnm.setGeometry(120, 80, 40, 40)
+        self.btnm.clicked.connect(self.click_event_m)
 
-        self.btn4.setGeometry(30, 130, 30, 30)
-        self.btn4.setText('4')
-        self.btn4.clicked.connect(self.click_event_4)
-
-        self.btn5.setGeometry(60, 130, 30, 30)
-        self.btn5.setText('5')
-        self.btn5.clicked.connect(self.click_event_5)
-
-        self.btn6.setGeometry(90, 130, 30, 30)
-        self.btn6.setText('6')
-        self.btn6.clicked.connect(self.click_event_6)
-
-        self.btn7.setGeometry(30, 160, 30, 30)
-        self.btn7.setText('7')
-        self.btn7.clicked.connect(self.click_event_7)
-
-        self.btn8.setGeometry(60, 160, 30, 30)
-        self.btn8.setText('8')
-        self.btn8.clicked.connect(self.click_event_8)
-
-        self.btn9.setGeometry(90, 160, 30, 30)
-        self.btn9.setText('9')
-        self.btn9.clicked.connect(self.click_event_9)
-
-        self.btn0.setGeometry(60, 190, 30, 30)
-        self.btn0.setText('0')
-        self.btn0.clicked.connect(self.click_event_0)
-
-        self.btns.setGeometry(120, 100, 30, 60)
-        self.btns.setText('+')
-        self.btns.clicked.connect(self.click_event_s)
-
-        self.btnr.setGeometry(120, 160, 30, 60)
-        self.btnr.setText('-')
-        self.btnr.clicked.connect(self.click_event_r)
-
-        self.btny.setGeometry(30, 190, 30, 30)
-        self.btny.setText('*')
+        self.btny = QPushButton('*', self)
+        self.btny.setGeometry(0, 160, 40, 40)
         self.btny.clicked.connect(self.click_event_y)
 
-        self.btnd.setGeometry(90, 190, 30, 30)
-        self.btnd.setText('/')
+        self.btnd = QPushButton('/', self)
+        self.btnd.setGeometry(80, 160, 40, 40)
         self.btnd.clicked.connect(self.click_event_d)
 
-        self.btnc.setGeometry(150, 70, 30, 30)
-        self.btnc.setText('C')
-        self.btnd.clicked.connect(self.click_event_c)
+        self.btnc = QPushButton('C', self)
+        self.btnc.setGeometry(120, 0, 40, 40)
+        self.btnc.clicked.connect(self.click_event_c)
 
         self.show()
 
-    def click_event_1(self):
-        global a
-        self.le.setText(self.le.text() + '1')
-        a = a + '1'
+    def click_event(self, j):
+        global numb
+        self.le.setText(self.le.text() + str(j))
+        numb += str(j)
 
-    def click_event_2(self):
-        global a
-        self.le.setText(self.le.text() + '2')
-        a = a + '2'
-
-    def click_event_3(self):
-        global a
-        self.le.setText(self.le.text() + '3')
-        a = a + '3'
-
-    def click_event_4(self):
-        global a
-        self.le.setText(self.le.text() + '4')
-        a = a + '4'
-
-    def click_event_5(self):
-        global a
-        self.le.setText(self.le.text() + '5')
-        a = a + '5'
-
-    def click_event_6(self):
-        global a
-        self.le.setText(self.le.text() + '6')
-        a = a + '6'
-
-    def click_event_7(self):
-        global a
-        self.le.setText(self.le.text() + '7')
-        a = a + '7'
-
-    def click_event_8(self):
-        global a
-        self.le.setText(self.le.text() + '8')
-        a = a + '8'
-
-    def click_event_9(self):
-        global a
-        self.le.setText(self.le.text() + '9')
-        a = a + '9'
-
-    def click_event_0(self):
-        global a
-        self.le.setText(self.le.text() + '0')
-        a = a + '0'
-
-    def click_event_s(self):
-        global pr, a
-        prov()
-        self.le.setText(self.le.text() + '+')
-        pr = 1
-
-    def click_event_r(self):
-        global pr, a
-        prov()
-        self.le.setText(self.le.text() + '-')
-        pr = 2
-
-    def click_event_y(self):
-        global pr, a
-        prov()
-        self.le.setText(self.le.text() + '*')
-        pr = 3
-
-    def click_event_d(self):
-        global pr, a
-        prov()
-        self.le.setText(self.le.text() + '/')
-        pr = 4
-
-    def click_event_c(self):
-        global pr, a, calc
-        self.le.setText('')
-        calc = 0
-        pr = 0
-        a = ''
-
-    def equal_click_event(self):
-        global pr, a, calc
+    def click_event_e(self):
+        global pd, numb, calc
         prov()
         self.le.setText(str(calc))
-        pr = 0
-        a = str(calc)
+        pd = 0
+        numb = str(calc)
         calc = 0
+
+    def click_event_p(self):
+        global pd, numb
+        prov()
+        self.le.setText(self.le.text() + '+')
+        pd = 1
+
+    def click_event_m(self):
+        global pd, numb
+        prov()
+        self.le.setText(self.le.text() + '-')
+        pd = 2
+
+    def click_event_y(self):
+        global pd, numb
+        prov()
+        self.le.setText(self.le.text() + '*')
+        pd = 3
+
+    def click_event_d(self):
+        global pd, numb
+        prov()
+        self.le.setText(self.le.text() + '/')
+        pd = 4
+
+    def click_event_c(self):
+        global pd, numb, calc
+        self.le.setText('')
+        calc = 0
+        pd = 0
+        numb = ''
 
 
 if __name__ == '__main__':
